3a354b7afae3dc86d9d76c6a4bb6e94f3451fb7d
Eduard Znakhurenko <zned003@gmail.com>
2024-12-15 20:19:31 +0100

    js (#13)

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,16 @@
-() => {
-    const refs = {
-      // Додати атрибут data-modal-open на кнопку відкриття
-      openModalBtn: document.querySelector("[data-menu-open]"),
-      // Додати атрибут data-modal-close на кнопку закриття
-      closeModalBtn: document.querySelector("[data-menu-close]"),
-      // Додати атрибут data-modal на бекдроп модалки
-      modal: document.querySelector("[data-menu]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
- }
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-menu-open]'),
+
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+
+    modal: document.querySelector('[data-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+  }
+})();
